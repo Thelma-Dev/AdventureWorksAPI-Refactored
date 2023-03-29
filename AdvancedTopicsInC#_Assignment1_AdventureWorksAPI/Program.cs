2ac50b73a191b9c36f1a73aff3a417c088aaa183
Seth Vandenbos <seth.vdbos@gmail.com>
2023-03-29 07:25:27 -0500

    Added Create Methods

--- AdvancedTopicsInC#_Assignment1_AdventureWorksAPI/Program.cs
+++ AdvancedTopicsInC#_Assignment1_AdventureWorksAPI/Program.cs
@@ -3,10 +3,23 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AdventureWorksLt2019Context>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksDb"));
+});
 var app = builder.Build();
 
-builder.Services.AddDbContext<AdventureWorksLt2019Context>(options => {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksDb"))
+app.MapGet("/address", (AdventureWorksLt2019Context db) =>
+{
+    return Results.Ok(new
+    {
+        Addresses = db.Addresses.ToList()
+    });
+});
+app.MapPost("/address", (AdventureWorksLt2019Context db, Address address) =>
+{
+    db.Add(address);
+    db.SaveChanges();
+    return Results.Ok();
 });
 
 app.Run();
