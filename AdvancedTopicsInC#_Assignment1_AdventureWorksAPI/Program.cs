4975b8b08bb5fcb3bffcbc2304a9ec1b15cc1eb6
Brick Lai <l279810259@gmail.com>
2023-03-30 10:07:28 -0500

    Customer Details and Address Details

--- AdvancedTopicsInC#_Assignment1_AdventureWorksAPI/Program.cs
+++ AdvancedTopicsInC#_Assignment1_AdventureWorksAPI/Program.cs
@@ -1,9 +1,13 @@
 using AdvancedTopicsInC__Assignment1_AdventureWorksAPI.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Drawing;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AdventureWorksLt2019Context>(options => {
+builder.Services.AddDbContext<AdventureWorksLt2019Context>(options =>
+{
     options.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksDb"));
 });
 var app = builder.Build();
@@ -67,5 +71,82 @@ app.MapPost("/salesorder/create", (AdventureWorksLt2019Context db, SalesOrderHea
     db.SaveChanges();
     return Results.Ok();
 });
+
+//---------------------------------------------------------------------------------------------------
+
+
+app.MapGet("/Customer/Details/{CustomerId}", (int CustomerId, AdventureWorksLt2019Context db) =>
+
+{
+
+    Customer customer = db.Customers.Include(a => a.CustomerAddresses)
+
+    .ThenInclude(b => b.Address)
+
+    .FirstOrDefault(c => c.CustomerId == CustomerId);
+
+    if (customer == null)
+
+    {
+        return Results.BadRequest("Customer does not exist.");
+    }
+    var address = customer.CustomerAddresses.Select(a => a.Address);
+
+    var customerAddress = new
+
+    {
+
+        Customer = customer,
+
+        Address = address
+
+    };
+
+    var options = new JsonSerializerOptions
+    {
+        ReferenceHandler = ReferenceHandler.Preserve
+    };
+
+    var serializer = System.Text.Json.JsonSerializer.Serialize(customerAddress, options);
+
+    return Results.Ok(serializer);
+
+});
+
+app.MapGet("/Address/Details/{AddressId}", (int AddressId, AdventureWorksLt2019Context db) =>
+{
+
+    Address address = db.Addresses.Include(a => a.CustomerAddresses)
+    .ThenInclude(b => b.Customer)
+
+    .FirstOrDefault(c => c.AddressId == AddressId);
+
+
+    if (address == null)
+    {
+        return Results.BadRequest("Address does not exist.");
+    }
+
+    var customer = address.CustomerAddresses.Select(a => a.Customer);
+
+    var customerAddress = new
+
+    {
+        Address = address,
+        Customer = customer
+    };
+
+    var options = new JsonSerializerOptions
+    {
+        ReferenceHandler = ReferenceHandler.Preserve
+    };
+
+    var serializer = System.Text.Json.JsonSerializer.Serialize(customerAddress, options);
+
+    return Results.Ok(serializer);
+});
+
+
+
 
 app.Run();
