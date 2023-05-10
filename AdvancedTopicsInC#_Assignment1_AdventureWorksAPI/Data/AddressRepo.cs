d9dbd93781e683ad6121aa9d8d603596b1b82509
inder <cheema.inder10@gmail.com>
2023-05-10 14:08:31 -0500

    Address Repo Created

---
+++ AdvancedTopicsInC#_Assignment1_AdventureWorksAPI/Data/AddressRepo.cs
@@ -0,0 +1,58 @@
+﻿using AdvancedTopicsInC__Assignment1_AdventureWorksAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvancedTopicsInC__Assignment1_AdventureWorksAPI.Data
+{
+    public class AddressRepo : IAddressRepo
+    {
+        private AdventureWorksLt2019Context _context;
+
+        public AddressRepo(AdventureWorksLt2019Context context)
+        {
+            _context = context;
+        }
+        public Address GetAddressById(int? id)
+        {
+            return _context.Addresses.Find(id);
+        }
+        public HashSet<Address> GetAddress()
+        {
+            return _context.Addresses.ToHashSet();
+        }
+        public void CreateAddress(Address address)
+        {
+            _context.Add(address);
+            _context.SaveChanges();
+        }
+
+        public void UpdateAddress(int id)
+        {
+            Address address = GetAddressById(id);
+            _context.Update(address);
+            _context.SaveChanges();
+        }
+
+        public void DeleteAddress(int id)
+        {
+            Address address = GetAddressById(id);
+            _context.Remove(address);
+            _context.SaveChanges();
+        }
+
+        private ICollection<Customer> GetCustomers()
+        {
+
+            return _context.Customers.ToList();
+        }
+
+        public ICollection<CustomerAddress> GetCustomerInAddress(int addressId)
+        {
+            Address address = GetAddressById(addressId);
+
+            ICollection<Customer> customer = GetCustomers();
+
+            return _context.CustomerAddresses.Where(ca => ca.AddressId == addressId && ca.CustomerId == 1).ToList();
+        }
+
+    }
+}
